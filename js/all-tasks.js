999202dff4f2b23a691f131a52c1be0fe46dc64f
Vadym Kozlov <70938824+kozlovvadym09@users.noreply.github.com>
2021-01-29 01:28:57 +0100

    Решает ДЗ 6.

---
+++ js/all-tasks.js
@@ -0,0 +1,106 @@
+'use strict'
+import users from "./users.js"
+
+// Task-1
+
+const getUserNames = users => {
+
+   return users.map(user => user.name);   
+};
+
+console.log(getUserNames(users));
+
+
+// Task-2
+
+const getUsersWithEyeColor = (users, color) => {
+   
+   return users.filter(user => user.eyeColor === color); 
+   
+}; 
+
+console.log(getUsersWithEyeColor(users, 'blue'));
+
+
+// Task-3 
+
+const getUsersWithGender = (users, gender) => {
+  return users.filter(user => user.gender === gender);
+};
+
+console.log(getUsersWithGender(users, 'male'));
+
+
+// Task-4 
+
+const getInactiveUsers = users => {
+   return users.filter(user => !user.isActive);
+};
+
+console.log(getInactiveUsers(users));
+
+
+// Task-5 
+
+const getUserWithEmail = (users, email) => {
+   return users.find(user => user.email === email); 
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); 
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); 
+
+
+// Task-6 
+
+const getUsersWithAge = (users, min, max) => {
+   return users.filter(user => min <= user.age && user.age <= max);
+};
+
+console.log(getUsersWithAge(users, 20, 30)); 
+console.log(getUsersWithAge(users, 30, 40));
+
+
+// Task-7 
+
+const calculateTotalBalance = users => {
+   return users.reduce((totalBalance, user) => totalBalance + user.balance, 0);
+};
+
+console.log(calculateTotalBalance(users));
+
+
+// Task-8 
+
+const getUsersWithFriend = (users, friendName) => {
+   return users.filter(user => user.friends.some(friend => friend === friendName));  
+};
+
+console.log(getUsersWithFriend(users, 'Briana Decker')); 
+console.log(getUsersWithFriend(users, 'Goldie Gentry'));
+
+
+// Task-9 
+
+const getNamesSortedByFriendsCount = users => {
+   const sorted = users
+      .sort((a, b) => a.friends.length - b.friends.length)
+      .map(user => user.name);
+   return sorted;
+};
+
+console.log(getNamesSortedByFriendsCount(users));
+
+
+// Task-10
+
+const getSortedUniqueSkills = users => {
+  return users
+    .reduce((allSkills, user) => {
+      allSkills.push(...user.skills);
+      return allSkills;
+    }, [])
+    .filter((user, index, arr) => arr.indexOf(user) === index)
+    .sort();
+};
+
+console.log(getSortedUniqueSkills(users));
